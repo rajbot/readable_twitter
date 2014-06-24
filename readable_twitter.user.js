95d3ed390413fbf464bb22f0bf58deaeb3efcfb4
rajbot <raj@archive.org>
2014-06-23 21:40:10 -0700

    mute the blue around the Compose new Tweet box

--- readable_twitter.user.js
+++ readable_twitter.user.js
@@ -18,6 +18,12 @@
         ".twitter-atreply > s {display: none}",
         ".twitter-hashtag > b {color: #8899a6}",
         ".twitter-hashtag > s {display: none}",
+
+        //mute the blue around the "Compose new Tweet" / "Reply to" box
+        ".home-tweet-box, .dm-tweetbox, .WebToast-box--altColor, .content-main .conversations-enabled .expansion-container .inline-reply-tweetbox {background-color: #eeeeee}",
+        ".u-bgUserColorLightest {background-color: #eeeeee !important}",
+
+        //handle expand + view media links
         ".expand-stream-item {color: #8899a6}",
         ".details > .Icon:before {color: #8899a6}",
 
